4c373a90c714fdfd21309c90e206ae688702ce01
Greg Caufield <greg@embeddedcoffee.ca>
2020-10-03 09:59:40 -0600

    Integrate System.Console library
    Pull in System.Console package for running the Cli

--- FitFileTools.Cli/Program.cs
+++ FitFileTools.Cli/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
 
 namespace FitFileTools.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static Command BuildConvertTcxCommand()
         {
-            Console.WriteLine("Hello World!");
+            var cmd = new Command("tcx")
+            {
+                new Argument<FileInfo>("input_file", description: "The path of the TCX file to convert")
+                {
+                    Arity = ArgumentArity.ExactlyOne
+                }.ExistingOnly()
+            };
+
+            // ReSharper disable once InconsistentNaming
+            cmd.Handler = CommandHandler.Create((FileInfo input_file) =>
+            {
+                Console.WriteLine($"The value for tcx file input is {input_file}");
+            });
+
+            return cmd;
+        }
+        static int Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Command("convert", "Convert a File to Fit.")
+                {
+                    BuildConvertTcxCommand()
+                }
+            };
+
+            return rootCommand.InvokeAsync(args).Result;
         }
     }
 }
